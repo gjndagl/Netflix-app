ca7441e1e1386821e236a9bddbbc95f23895e551
gjndagl <106946749+gjndagl@users.noreply.github.com>
2023-07-02 14:57:43 +0530

    Added routes for deleting a user , getting a single user, getting all the users

--- api/routes/users.js
+++ api/routes/users.js
@@ -13,8 +13,14 @@ router.put("/:id",verify,async(req,res)=>{
                 process.env.SECRET_KEY
             ).toString();
         } try{
-           const updatedUser = await User.findByIdAndUpdate(req.params.id,{$set:req.body});
-           res.status(200).json({success:true, updatedUser});
+           const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set:req.body
+            },
+             {new:true}
+            );
+           res.status(200).json( updatedUser);
         }catch(err){
            res.status(500).json(err);
         }
@@ -29,8 +35,64 @@ router.put("/:id",verify,async(req,res)=>{
 });
 
 //DELETE
+router.delete("/:id",verify,async(req,res)=>{
+    if(req.user.id === req.params.id || req.user.isAdmin)
+    {
+      try{
+           await User.findByIdAndDelete(req.params.id);
+          res.status(200).json("User has been deleted...");
+       }catch(err){
+          res.status(500).json(err);
+       }
+
+    }
+    else
+    {
+       res.status(403).json("You can delete only your account!");
+    }
+
+   
+});
+
+
 //GET
+router.get("/find/:id",verify,async(req,res)=>{
+    
+      try{
+          const user= await User.findById(req.params.id);
+          const {password, ...info}=user._doc
+          res.status(200).json(info);
+       }catch(err){
+          res.status(500).json(err);
+       }
+
+});
+
+
+
 //GET ALL
+
+router.get("/",verify,async(req,res)=>{
+    const query=req.query.new;
+    if( req.user.isAdmin)
+    {
+      try{
+          const users=query?await User.find().limit(10) : await User.find();
+          res.status(200).json(users);
+       }catch(err){
+          res.status(500).json(err);
+       }
+
+    }
+    else
+    {
+       res.status(403).json("You can not allowed to see all users!");
+    }
+
+   
+});
+
+
 //GET USER STATS
 
 module.exports=router;
