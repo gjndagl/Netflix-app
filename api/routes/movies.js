0f5794d08d2b1c9db2e7cbade7f0d365d0847f4a
gjndagl <106946749+gjndagl@users.noreply.github.com>
2023-07-02 20:35:07 +0530

    Made small changes

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -63,4 +63,22 @@ router.delete("/:id",verify,async(req,res)=>{
     }
 });
 
+//GET
+
+router.delete("/:id",verify,async(req,res)=>{
+    if(req.user.isAdmin)
+    {
+       try{
+        await Movie.findByIdAndDelete(req.params.id);
+         res.status(200).json("The Movie has been deleted");
+       }catch(err){
+           res.status(500).json(err);
+       }
+    }
+    else
+    {
+       res.status(403).json("You are not allowed!");
+    }
+});
+
 module.exports=router;
