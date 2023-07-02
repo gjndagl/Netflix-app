45340dc762c8343c00aaad468d2a578344125c75
gjndagl <106946749+gjndagl@users.noreply.github.com>
2023-07-02 20:34:27 +0530

    Added movie routes

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -3,7 +3,7 @@ const Movie=require("../models/Movie");
 const verify=require('../verifyToken');
 
 //CREATE
-router.put("/",verify,async(req,res)=>{
+router.post("/",verify,async(req,res)=>{
      if(req.user.isAdmin)
      {
         const newMovie=new Movie(req.body);
@@ -21,6 +21,46 @@ router.put("/",verify,async(req,res)=>{
      }
 
     
+});
+
+//UPDATE
+router.put("/:id",verify,async(req,res)=>{
+    if(req.user.isAdmin)
+    {
+       try{
+         const updatedMovie=await Movie.findByIdAndUpdate(req.params.id,
+            {
+                $set:req.body
+            },
+            { new: true}
+            );
+         res.status(200).json(updatedMovie);
+       }catch(err){
+           res.status(500).json(err);
+       }
+    }
+    else
+    {
+       res.status(403).json("You are not allowed!");
+    }
+});
+
+//DELETE
+
+router.delete("/:id",verify,async(req,res)=>{
+    if(req.user.isAdmin)
+    {
+       try{
+        await Movie.findByIdAndDelete(req.params.id);
+         res.status(200).json("The Movie has been deleted");
+       }catch(err){
+           res.status(500).json(err);
+       }
+    }
+    else
+    {
+       res.status(403).json("You are not allowed!");
+    }
 });
 
 module.exports=router;
