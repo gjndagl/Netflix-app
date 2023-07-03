409139bb265e7bfeada4d09c13382b6d55d25d08
gjndagl <106946749+gjndagl@users.noreply.github.com>
2023-07-03 14:27:26 +0530

    Completed movie routes

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -65,7 +65,7 @@ router.delete("/:id",verify,async(req,res)=>{
 
 //GET
 
-router.get("/:id",verify,async(req,res)=>{
+router.get("/find/:id",verify,async(req,res)=>{
    
        try{
         const movie=await Movie.findById(req.params.id);
@@ -76,4 +76,43 @@ router.get("/:id",verify,async(req,res)=>{
     
 });
 
+//GET RANDOM
+
+router.get("/random",verify,async(req,res)=>{
+   const type=req.query.type;
+    try{
+      if(type==="series"){
+        movie=await Movie.aggregate([
+            { $match:{isSeries:true}}  ,
+            { $sample: {size:1}}
+        ]);
+       
+      }
+      else{
+        movie=await Movie.aggregate([
+            { $match:{isSeries:false}}  ,
+            { $sample: {size:1}}
+        ]);
+      }
+      res.status(200).json(movie);
+    }catch(err){
+        res.status(500).json(err);
+    }
+ 
+});
+
+//GET ALL
+
+router.get("/", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+      try {
+        const movies = await Movie.find();
+        res.status(200).json(movies.reverse());
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You are not allowed!");
+    }
+  });
 module.exports=router;
