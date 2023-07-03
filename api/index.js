6ce8a8ef0dce49496e5fb179097c2e9502f7a705
gjndagl <106946749+gjndagl@users.noreply.github.com>
2023-07-03 15:25:23 +0530

    Added List Routes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const dotenv=require("dotenv");
 const authRoute=require("./routes/auth");
 const userRoute=require("./routes/users");
 const movieRoute=require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 dotenv.config();
 
@@ -32,6 +33,7 @@ app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
 app.use("/api/movies",movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(3000,()=>{
     console.log("Backend server is running");
